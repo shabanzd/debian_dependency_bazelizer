5e777f57546308c52d91c868d672d99100cc5637
shabanzd <shaban_ziad@hotmail.com>
2023-04-23 20:01:15 +0200

    added input file reader

--- src/main.py
+++ src/main.py
@@ -1,5 +1,51 @@
+from typing import Final, Set
+from pathlib import Path
+
+import dataclasses
+import os
+
+BAZEL_WORKSPACE_DIR: Final = os.environ["BUILD_WORKSPACE_DIRECTORY"]
+DEB_PACKAGE_IN: Final = Path().joinpath(
+    BAZEL_WORKSPACE_DIR, "src", "deb_packages.in"
+)
+
+@dataclasses.dataclass(frozen=True)
+class PackageMetadata:
+    name: str
+    arch: str
+    version: str
+
+def _get_package_metadata(pinned_package: str) -> PackageMetadata:
+    if pinned_package.count(":") != 1 or pinned_package.count("=") > 1:
+        raise ValueError("Entry has unexpected format, expected format: name:arch=version or name:arch")
+    
+    # entries format: name:arch=version
+    name, arch_version = pinned_package.split(":")
+    version = ""
+    arch = arch_version
+
+    if "=" in arch_version:
+        arch, version = arch_version.split("=")
+    
+    if not version:
+        # get version from registry
+        print("implicit version deduction not yet implemented")
+
+    return PackageMetadata(name=name, arch=arch, version=version)
+        
+
+def _get_input_file_entries() -> Set[PackageMetadata]:
+    with open(DEB_PACKAGE_IN, "r") as input_file:
+        entries = set(input_file.read().splitlines())
+    
+    return {_get_package_metadata(entry) for entry in entries if entry and not entry.startswith("#")}
+
+
+
+        
+
 def main():
-    print("hello world")
+    print(_get_input_file_entries())
 
 if __name__ == "__main__":
     main()
