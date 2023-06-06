d12e7ba7830803852e113f292b8d74ea2f3d29e8
shabanzd <shaban_ziad@hotmail.com>
2023-06-06 17:01:38 +0200

    support multiple input files

--- src/read_input_files.py
+++ src/read_input_files.py
@@ -1,9 +1,10 @@
 """File containing an input file reader for deb packages."""
-from typing import Set
+from typing import List, Set
 from pathlib import Path
 
 from src.package import PackageMetadata
 from src.get_package_version import get_package_version
+
 
 def _get_package_metadata(registry_path: Path, pinned_package: str) -> PackageMetadata:
     if pinned_package.count(":") != 1 or pinned_package.count("=") > 1:
@@ -23,12 +24,17 @@ def _get_package_metadata(registry_path: Path, pinned_package: str) -> PackageMe
 
     return PackageMetadata(name=name, arch=arch, version=version)
 
-def read_input_file(registry_path: Path, input_file: Path) -> Set[PackageMetadata]:
-    """Reads input file and returns a Set of PackageMetadatas."""
-    entries = input_file.read_text().splitlines()
+
+def read_input_files(
+    registry_path: Path, input_files: List[Path]
+) -> Set[PackageMetadata]:
+    """Reads input files and returns a Set of PackageMetadatas."""
+    entries: Set[str] = set()
+    for input_file in input_files:
+        entries.update(set(input_file.read_text().splitlines()))
 
     return {
         _get_package_metadata(registry_path=registry_path, pinned_package=entry)
-        for entry in entries if entry and not entry.startswith("#")
+        for entry in entries
+        if entry and not entry.startswith("#")
     }
-   
