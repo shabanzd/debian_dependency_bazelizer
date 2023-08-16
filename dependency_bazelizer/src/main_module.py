542cc71f833f4894b42efafb63af853d93a87b7f
Ziad Shaban <shaban_ziad@hotmail.com>
2023-08-16 16:32:51 +0200

    adapt changes to module extension

--- dependency_bazelizer/src/main_module.py
+++ dependency_bazelizer/src/main_module.py
@@ -1,38 +1,19 @@
 from pathlib import Path
-from typing import Final, Dict
+from typing import Final
 
-import json
 import os
 
 from dep_bazelizer_config import get_deb_packages_path, get_storage_config_file_path
 from src.bazelize_deps import bazelize_deps
 from src.read_input_files import read_input_files
+from src.storage import Storage
 
 BAZEL_WORKSPACE_DIR: Final = (
     os.environ.get("BUILD_WORKSPACE_DIRECTORY")
     or os.environ.get("TEST_UNDECLARED_OUTPUTS_DIR")
     or os.environ.get("TEST_TMPDIR")
 )
-MANDATORY_CONFIGS: Final = ["upload_bucket", "upload_url"]
 
-
-def _get_storage_config(json_config_file: Path):
-    if json_config_file.suffix != ".json":
-        path_str = str(json_config_file)
-        raise ValueError(f"The file '{path_str}' must be a .json file.")
-
-    with open(json_config_file, encoding="utf-8") as json_config:
-        storage_config = json.load(json_config)
-
-    _verify_storage_config(storage_config)
-
-    return storage_config
-
-
-def _verify_storage_config(configs: Dict[str, str]):
-    for mandatory_config in MANDATORY_CONFIGS:
-        if mandatory_config not in configs:
-            raise ValueError(f"missing mandatory config: {mandatory_config}.")
 
 def main_module():
     """Turns input deb packages into modules referenced by a local registry."""
@@ -40,7 +21,7 @@ def main_module():
 
     bazelize_deps(
         registry_path=registry_path,
-        storage_config=_get_storage_config(Path(get_storage_config_file_path())),
+        storage=Storage(Path(get_storage_config_file_path())),
         input_package_metadatas=read_input_files(
             registry_path=registry_path, input_files=[Path(get_deb_packages_path())]
         )
