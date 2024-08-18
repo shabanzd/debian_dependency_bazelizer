4bd909db70b03f40ca0041cfa1b16a26a4961a9e
Ziad Shaban <shaban_ziad@hotmail.com>
2024-08-18 18:28:34 +0200

    fix tests on main (#67)
    Co-authored-by: ziad-fernride <ziad.shaban@fernride.com>

--- tests/test_read_input_files.py
+++ tests/test_read_input_files.py
@@ -20,9 +20,7 @@ def test_correct_input_file():
                     registry_path=Path("../resources/registry"),
                     input_files=[Path("../_main/tests/resources/correct_input.in").resolve()]
                 )
-    assert len(metadatas) == 5
-    assert PackageMetadata(name="iproute2", arch="arm64", version="1.2.3") in metadatas
-    assert PackageMetadata(name="iproute2", arch="arm64", version="1:1.3.4") in metadatas
+    assert len(metadatas) == 3
     assert PackageMetadata(name="iproute2", arch="amd64", version="1:1.2.4") in metadatas
     assert PackageMetadata(name="patchelf", arch="amd64", version="4.5.6") in metadatas
     assert PackageMetadata(name="patchelf", arch="amd64", version="5.6.7") in metadatas
@@ -36,12 +34,21 @@ def test_duplicate_input_files():
                                 Path("../_main/tests/resources/correct_input.in").resolve()
                                 ]
                 )
-    assert len(metadatas) == 5
-    assert PackageMetadata(name="iproute2", arch="arm64", version="1.2.3") in metadatas
-    assert PackageMetadata(name="iproute2", arch="arm64", version="1:1.3.4") in metadatas
+    assert len(metadatas) == 3
     assert PackageMetadata(name="iproute2", arch="amd64", version="1:1.2.4") in metadatas
     assert PackageMetadata(name="patchelf", arch="amd64", version="4.5.6") in metadatas
     assert PackageMetadata(name="patchelf", arch="amd64", version="5.6.7") in metadatas
+
+def test_arm_input_files():
+    """Tests arm input files."""
+    with pytest.raises(ValueError) as e:
+        read_input_files(
+                    registry_path=Path("../resources/registry"),
+                    input_files=[
+                                Path("../_main/tests/resources/arm_input.in").resolve(),
+                                ]
+                )
+    assert str(e.value) == "Unsupported architecture arm64. Only amd64 arch is supported so far."
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__] + sys.argv[1:]))
